ae39481c475217499ae183cd00f04e3ed64110e8
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-16 04:44:38 +0800

    Add project files.

---
+++ CONSOLESUNITTEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CONSOLESUNITTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
