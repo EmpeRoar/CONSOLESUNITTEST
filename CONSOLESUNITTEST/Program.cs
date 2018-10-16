b202090e5c687a4fd6512708dec3cceeb3cb1419
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-16 09:13:32 +0800

    Turtle

--- CONSOLESUNITTEST/Program.cs
+++ CONSOLESUNITTEST/Program.cs
@@ -2,11 +2,51 @@
 
 namespace CONSOLESUNITTEST
 {
+
+    public interface ITurtleState
+    {
+        int XPos { get; set; }
+        int YPos { get; set; }
+    }
+
+    public class TurtleState : ITurtleState
+    {
+        public int XPos { get; set; } = 0;
+        public int YPos { get; set; } = 0;
+    }
+
+    public interface ITurtle
+    {
+        void Move();
+        ITurtleState GetState();
+    }
+
+    public class Turtle : ITurtle
+    {
+        ITurtleState _turtleState;
+        public Turtle(ITurtleState turtleState)
+        {
+            _turtleState = turtleState;
+        }
+
+        public ITurtleState GetState() => _turtleState;
+
+        public void Move()
+        {
+            _turtleState.XPos++;
+            _turtleState.YPos++;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var turtleState = new TurtleState();
+            var turtle = new Turtle(turtleState);
+            turtle.Move();
+            turtle.Move();
+            Console.WriteLine($"{turtle.GetState().XPos}-{turtle.GetState().YPos}");
+            Console.ReadLine();
         }
     }
 }
---
+++ UnitTests/TurtleTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TurtleTest
+    {
+        public TurtleTest()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
